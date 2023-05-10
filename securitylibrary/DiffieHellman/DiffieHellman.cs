2a00e38ab63a0c101f3b07baaadf236fd6442200
Alaa Mostafa <alaamostafayamany@gmail.com>
2023-05-10 18:16:05 +0300

    DiffieHellman

--- securitylibrary/DiffieHellman/DiffieHellman.cs
+++ securitylibrary/DiffieHellman/DiffieHellman.cs
@@ -8,9 +8,23 @@ namespace SecurityLibrary.DiffieHellman
 {
     public class DiffieHellman
     {
+        int Power_Mod(int actualNum, int powered, int qq)
+        {
+            int ans = 1;
+            for (int i = 0; i < powered; i++)
+            {
+                ans = (actualNum * ans) % qq;
+            }
+
+            return ans;
+        }
         public List<int> GetKeys(int q, int alpha, int xa, int xb)
         {
-            throw new NotImplementedException();
+            int ya = Power_Mod(alpha, xa, q);
+            int yb = Power_Mod(alpha, xb, q);
+            int ans1 = Power_Mod(yb, xa, q);
+            int ans2 = Power_Mod(ya, xb, q);
+            return new List<int>() { ans1, ans2 };
         }
     }
 }
