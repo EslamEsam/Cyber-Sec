dc9a09568872f83d5367e6c2216cc400fbde62cd
Alaa Mostafa <alaamostafayamany@gmail.com>
2023-05-10 17:48:30 +0300

    RSA modification

--- securitylibrary/RSA/RSA.cs
+++ securitylibrary/RSA/RSA.cs
@@ -1,4 +1,5 @@
 ﻿using SecurityLibrary.AES;
+using SecurityLibrary.ElGamal;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,25 +10,21 @@ namespace SecurityLibrary.RSA
 {
     public class RSA
     {
-        public int getMod(int a, int b, int n)
+        public int Power_Mod(int actualNum, int powered, int qq)
         {
-            long ans = 1, tempM = a;
-            while (b > 0)
+            int ans = 1;
+            for (int i = 0; i < powered; i++)
             {
-                if (b % 2 == 1)
-                {
-                    ans = (ans * tempM) % n;
-                }
-                tempM = (tempM * tempM) % n;
-                b /= 2;
+                ans = (actualNum * ans) % qq;
             }
-            return (int)(ans % n);
+
+            return ans;
         }
         public int Encrypt(int p, int q, int M, int e)
         {
             int n = p * q;
             int euler = (p - 1) * (q - 1);
-            int ans = getMod(M, e, n);
+            int ans =  Power_Mod(M, e, n);
             return ans % n;
 
         }
@@ -38,7 +35,7 @@ namespace SecurityLibrary.RSA
             int euler = (p - 1) * (q - 1);
             ExtendedEuclid ee = new ExtendedEuclid();
             int d = ee.GetMultiplicativeInverse(e, euler);
-            int ans = getMod(C, d, n);
+            int ans = Power_Mod(C, d, n);
             return ans % n;
         }
     }
